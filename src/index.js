88ae5a745c8b9a978a2a293c640bd784c2201f9f
Tobias Bertera <tobiasbertera@gmail.com>
2023-10-04 16:06:43 -0300

    Add functions that create DOM elements
    headerMaker, mainMaker and footerMaker that create
    dom elements.
    
    Also, added some JS files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log("ola k ase");
+let mainDiv = document.querySelector("#content");
+
+
+function headerMaker() {
+    let header = document.createElement("header");
+    header.classList.add("header");
+    mainDiv.appendChild(header)
+    let title = document.createElement("h1")
+    title.classList.add("title");
+    title.textContent = "La Parrilla";
+    header.appendChild(title);
+    let home = document.createElement("div")
+    home.classList.add("home")
+    home.textContent = "HOME"
+    header.appendChild(home)
+    let menu = document.createElement("div")
+    menu.classList.add("menu")
+    menu.textContent = "MENU"
+    header.appendChild(menu)
+    let contact = document.createElement("div")
+    contact.classList.add("contact")
+    contact.textContent = "CONTACT"
+    header.appendChild(contact)
+
+}
+
+function mainMaker(){
+    let main = document.createElement("main");
+    main.classList.add("main");
+    mainDiv.appendChild(main);
+}
+
+function footerMaker(){
+    let footer = document.createElement("footer");
+    footer.classList.add("footer");
+    footer.textContent = "totober© 2023";
+    mainDiv.appendChild(footer);
+}
+
+
+headerMaker()
+mainMaker()
+footerMaker()
---
+++ src/website.js
@@ -0,0 +1,14 @@
+
+function headerMaker() {
+    let header = document.createElement("header");
+    console.log(header)
+
+}
+
+function mainMaker(){
+
+}
+
+function footerMaker(){
+
+}
